4713be5e1ca791b3125ea0655dbc6f4f71f9d8d5
Logan Garrett <logan@Logans-MacBook-Pro.local>
2016-01-13 23:59:27 -0500

    loop

--- main.rb
+++ main.rb
@@ -13,35 +13,39 @@ plotly = PlotLy.new('LoganGarrett', ENV['plotly_api_key'])
 Temperature::DS18B20.new(hardware_id: '28-00000520ab0c', name: 'Yeti Colster')
 Temperature::DS18B20.new(hardware_id: '28-0000052100e5', name: 'Thermos Can Insulator')
 
-Temperature::DS18B20.all_meters.each do |meter|
-  puts "#{meter.name} - #{meter.read}"
-end
 
-data = [
-  {
-    x: [now],
-    y: [Temperature::DS18B20.all_meters[0].read],
-    name: Temperature::DS18B20.all_meters[0].name,
-    marker: { color: 'rgb(55, 83, 109)' }
-  },
-  {
-    x: [now],
-    y: [Temperature::DS18B20.all_meters[1].read],
-    name: Temperature::DS18B20.all_meters[1].name,
-    marker: { color: 'rgb(26, 118, 255)' }
-  },
-]
+loop do
+  Temperature::DS18B20.all_meters.each do |meter|
+    puts "#{meter.name} - #{meter.read}"
+  end
 
-args = {
-  filename: 'temperature',
-  fileopt: 'extend',
-  style: { type: 'scatter' },
-  layout: {
-    title: 'Temperature'
-  },
-  world_readable: true
-}
+  data = [
+    {
+      x: [now],
+      y: [Temperature::DS18B20.all_meters[0].read],
+      name: Temperature::DS18B20.all_meters[0].name,
+      marker: { color: 'rgb(55, 83, 109)' }
+    },
+    {
+      x: [now],
+      y: [Temperature::DS18B20.all_meters[1].read],
+      name: Temperature::DS18B20.all_meters[1].name,
+      marker: { color: 'rgb(26, 118, 255)' }
+    },
+  ]
 
-plotly.plot(data, args) do |response|
-  puts response['url']
+  args = {
+    filename: 'temperature',
+    fileopt: 'extend',
+    style: { type: 'scatter' },
+    layout: {
+      title: 'Temperature'
+    },
+    world_readable: true
+  }
+
+  plotly.plot(data, args) do |response|
+    puts response['url']
+  end
+  sleep 60
 end
