b13677f97a228639312cab4bd2f87d28442ca5ee
Logan Garrett <logan@Logans-MacBook-Pro.local>
2016-01-14 21:28:40 -0500

    sparkfun data

--- main.rb
+++ main.rb
@@ -3,9 +3,11 @@ require 'plotly'
 require 'time'
 require './lib/ds18b20.rb'
 require 'yaml'
-
+require 'rest-client'
 def now
-  Time.parse(Time.now.to_s).strftime("%Y-%m-%d %H:%M:%S")
+  #2016-01-15T02:13:00.388Z
+  t = Time.now
+  t.iso8601
 end
 
 #plotly = PlotLy.new('LoganGarrett', ENV['plotly_api_key'])
@@ -19,7 +21,7 @@ end
 
 def auto_config
   Temperature::DEVICES.each do |device|
-    Temperature::DS18B20.new(hardware_id: device, name: 'Yeti Colster')
+    Temperature::DS18B20.new(hardware_id: device)
   end
 end
 
@@ -29,45 +31,62 @@ def read_scopes
   end
 end
 
-def two_scope
-  Temperature::DS18B20.new(hardware_id: '28-00000520ab0c', name: 'Yeti Colster')
-  Temperature::DS18B20.new(hardware_id: '28-0000052100e5', name: 'Thermos Can Insulator')
-  loop do
-    Temperature::DS18B20.all_meters.each do |meter|
-      puts "#{meter.name} - #{meter.read}"
-    end
+# def two_scope
+#   Temperature::DS18B20.new(hardware_id: '28-00000520ab0c', name: 'Yeti Colster')
+#   Temperature::DS18B20.new(hardware_id: '28-0000052100e5', name: 'Thermos Can Insulator')
+#   loop do
+#     Temperature::DS18B20.all_meters.each do |meter|
+#       puts "#{meter.name} - #{meter.read}"
+#     end
+#
+#     data = [
+#       {
+#         x: [now],
+#         y: [Temperature::DS18B20.all_meters[0].read],
+#         name: Temperature::DS18B20.all_meters[0].name,
+#         marker: { color: 'rgb(55, 83, 109)' }
+#         stream:{ token: token}
+#       },
+#       {
+#         x: [now],
+#         y: [Temperature::DS18B20.all_meters[1].read],
+#         name: Temperature::DS18B20.all_meters[1].name,
+#         marker: { color: 'rgb(26, 118, 255)' }
+#         stream:{ token: token}
+#       },
+#     ]
+#
+#     args = {
+#       filename: 'temperature',
+#       fileopt: 'extend',
+#       style: { type: 'scatter' },
+#       layout: {
+#         title: 'Temperature'
+#       },
+#       world_readable: true
+#     }
+#
+#     plotly.plot(data, args) do |response|
+#       puts response['url']
+#     end
+#     sleep 60
+#   end
+# end
 
-    data = [
-      {
-        x: [now],
-        y: [Temperature::DS18B20.all_meters[0].read],
-        name: Temperature::DS18B20.all_meters[0].name,
-        marker: { color: 'rgb(55, 83, 109)' }
-      },
-      {
-        x: [now],
-        y: [Temperature::DS18B20.all_meters[1].read],
-        name: Temperature::DS18B20.all_meters[1].name,
-        marker: { color: 'rgb(26, 118, 255)' }
-      },
-    ]
+def log_data
+  endpoint = 'http://data.sparkfun.com/input/'
+  public_key = 'MGWW688WnRF6wgd8Zgz9'
+  private_key = ENV['sparkfun_private_key']
+  data_string = "#{endpoint}/input/#{public_key}?private_key=#{private_key}&timestamp=#{now}"
 
-    args = {
-      filename: 'temperature',
-      fileopt: 'extend',
-      style: { type: 'scatter' },
-      layout: {
-        title: 'Temperature'
-      },
-      world_readable: true
-    }
-
-    plotly.plot(data, args) do |response|
-      puts response['url']
-    end
-    sleep 60
+  Temperature::DS18B20.all_meters.each do |meter|
+    reading = "&sensor#{meter.tag_number}=#{meter.read}"
+    data_string << reading
+    puts data_string
   end
+  RestClient.get data_string
 end
 
 file_config
 read_scopes
+log_data
